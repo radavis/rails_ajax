f15a13050e7557b9a2393fc7fdca01662354084b
Richard Davis <rdavis@digitallumens.com>
2014-02-25 10:35:09 -0500

    remove items from the page without a full refresh

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -60,7 +60,8 @@ class TasksController < ApplicationController
     @task.destroy
     respond_to do |format|
       format.html { redirect_to tasks_url }
-      format.json { head :no_content }
+      format.js
+      #format.json { head :no_content }
     end
   end
 
