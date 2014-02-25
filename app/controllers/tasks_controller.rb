6668ef73ca4e0996fa0a1875c90acf219ff7f8a2
Richard Davis <rdavis@digitallumens.com>
2014-02-25 10:28:04 -0500

    add tasks to page without full page refresh

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,8 +30,9 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @task }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully created.' }
+        format.js
+        #format.json { render action: 'show', status: :created, location: @task }
       else
         format.html { render action: 'new' }
         format.json { render json: @task.errors, status: :unprocessable_entity }
