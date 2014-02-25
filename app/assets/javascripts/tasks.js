0898a327d96192154dd752a1ec82b2da8869a96d
Richard Davis <rdavis@digitallumens.com>
2014-02-25 11:04:20 -0500

    fully functional

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $('.edit_task input[type=submit]').remove();
-  $('.edit_task input[type=checkbox]').click(function() {
+  $('.edit_task').submitOnCheck();
+});
+
+jQuery.fn.submitOnCheck = function() {
+  this.find('input[type=submit]').remove();  // remove 'update' button
+  this.find('input[type=checkbox]').click(function() {
     $(this).parent('form').submit();
   });
-});
+  return this;  // return the element so we can chain functions
+};
