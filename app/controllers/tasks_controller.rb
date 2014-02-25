d4218d14f58ab103fde7332ac7a0a257d254ec6c
Richard Davis <rdavis@digitallumens.com>
2014-02-25 10:20:02 -0500

    jQuery code to handle form insertion into page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,9 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    #@tasks = Task.all
+    @complete_tasks = Task.where(complete: true)
+    @incomplete_tasks = Task.where(complete: false)
   end
 
   # GET /tasks/1
