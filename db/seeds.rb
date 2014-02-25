a2db34090ca2442f16b0b69e747bccae6da69210
Richard Davis <rdavis@digitallumens.com>
2014-02-25 09:19:36 -0500

    model, migration, and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+todo = [ 
+  ['wash clothes', false], 
+  ['wash dishes', false], 
+  ['hijack bus', false],
+  ['clean room', true],
+  ['make dinner', false]
+]
+
+todo.each do |task_name, completed|
+  Task.find_or_create_by_name(name: task_name, complete: completed)
+end
---
+++ spec/views/tasks/edit.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "tasks/edit" do
+  before(:each) do
+    @task = assign(:task, stub_model(Task,
+      :name => "MyString",
+      :complete => false
+    ))
+  end
+
+  it "renders the edit task form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", task_path(@task), "post" do
+      assert_select "input#task_name[name=?]", "task[name]"
+      assert_select "input#task_complete[name=?]", "task[complete]"
+    end
+  end
+end
