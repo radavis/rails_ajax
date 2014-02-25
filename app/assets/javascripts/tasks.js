239ff92ef6e55e02ea6f2078d5a6f0bd0992d6c2
Richard Davis <rdavis@digitallumens.com>
2014-02-25 10:46:06 -0500

    remove 'update' button and submit when checkbox is ticked

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.edit_task input[type=submit]').remove();
+  $('.edit_task input[type=checkbox]').click(function() {
+    $(this).parent('form').submit();
+  });
+});
